b3c9db548f9f8f3c091aa3f6f048ee4015be0279
Samiul Alam Rafi <sarafi7814@gmail.com>
2025-05-05 22:05:11 +0600

    Introduction

---
+++ Lab1/Lab1/Controllers/PersonController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lab1.Controllers
+{
+    public class PersonController : Controller
+    {
+        // GET: Person
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult List()
+        {
+            return View();
+        }
+    }
+}
