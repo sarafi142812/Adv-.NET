50df0527ec96d301d55c4f91b8beb858dd5188e8
Samiul Alam Rafi <sarafi7814@gmail.com>
2025-06-17 21:37:37 +0600

    CRUD

--- Lab1/Lab1/Controllers/PersonController.cs
+++ Lab1/Lab1/Controllers/PersonController.cs
@@ -16,6 +16,21 @@ namespace Lab1.Controllers
 
         public ActionResult List()
         {
+            int a = 10;
+            int b = 20;
+            int c = a + b;
+
+            ViewBag.a = a;
+            ViewBag.b = b;
+            ViewBag.c = c;
+
+            string[] names = new string[3];
+            names[0] = "John";
+            names[1] = "Rafi";
+            names[2] = "Rezuan";
+
+            ViewBag.Name = names;
+
             return View();
         }
     }
---
+++ ORM/ORM/Controllers/StudentController.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ORM.Models;
+
+namespace ORM.Controllers
+{
+    public class StudentController : Controller
+    {
+        // GET: Student
+        
+        public ActionResult Index()
+        {
+            UMS_AEntities db = new UMS_AEntities();
+            var data = db.Students.ToList();
+            return View(data);
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Student s)
+        {
+            UMS_AEntities db = new UMS_AEntities();
+            db.Students.Add(s);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+            
+        }
+
+        public ActionResult Edit(int Id)
+        {
+            UMS_AEntities db = new UMS_AEntities();
+            var student = (from st in db.Students
+                           where st.Id == Id
+                           select st).First();
+            return View(student);
+        }
+        [HttpPost]
+        public ActionResult Edit(Student s)
+        {
+            using (UMS_AEntities db = new UMS_AEntities())
+            {
+                var entity = (from st in db.Students
+                              where st.Id == s.Id
+                              select st).FirstOrDefault();
+               /* entity.Name = s.Name;
+                entity.Dob = s.Dob;
+                entity.Gender = s.Gender;
+                entity.Cgpa = s.Cgpa;*/
+                db.Entry(entity).CurrentValues.SetValues(s); // This will update all properties of the entity with the values from the model
+                db.SaveChanges(); 
+                return RedirectToAction("Index");
+            }
+                
+        }
+        public ActionResult Delete(int id)
+        {
+            using (UMS_AEntities db = new UMS_AEntities())
+            {
+                var student = (from st in db.Students
+                               where st.Id == id
+                               select st).FirstOrDefault();
+                return View(student);
+            }
+        }
+
+        [HttpPost]
+        public ActionResult Delete(Student s)
+        {
+            using (UMS_AEntities db = new UMS_AEntities())
+            {
+                var entity = (from st in db.Students
+                              where st.Id == s.Id
+                              select st).FirstOrDefault();
+
+                if (entity != null)
+                {
+                    db.Students.Remove(entity);
+                    db.SaveChanges();
+                }
+
+                return RedirectToAction("Index");
+            }
+        }
+
+
+
+
+    }
+
+
+}
